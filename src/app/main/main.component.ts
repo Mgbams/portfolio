a9d2a273ff7451e423ad9d5710e9666991038c19
mgbams <cmgbams@gmail.com>
2021-02-20 18:05:41 +0100

    project cards

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -31,23 +31,23 @@ export class MainComponent implements OnInit {
       $("#mainListDiv").fadeIn();
     });
 
-  // Bounce logic added to navbar links
-  $(function(){
-    var str = '#len'; //increment by 1 up to 1-nelemnts
-    $(document).ready(function(){
-      let i : number;
-      let stop: number;
-      i = 1;
-      stop = 4; //num elements
-      setInterval(function(){
-        if (i > stop){
-          return;
-        }
-        $('#len'+(i++)).toggleClass('bounce');
-      }, 500)
+    // Bounce logic added to navbar links
+    $(function(){
+      var str = '#len'; //increment by 1 up to 1-nelemnts
+      $(document).ready(function(){
+        let i : number;
+        let stop: number;
+        i = 1;
+        stop = 4; //num elements
+        setInterval(function(){
+          if (i > stop){
+            return;
+          }
+          $('#len'+(i++)).toggleClass('bounce');
+        }, 500)
+      });
     });
-  });
-  
+   
   }
 
 }
